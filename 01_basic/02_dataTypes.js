a1d912fdd5bacbedc1194cd5007ddc01b616cefa
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-01-10 18:10:29 +0000

    datatypes

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -2,4 +2,15 @@
 
 
 let name ="Akash";
+let age =24;
+
 console.log("name");
+// Data types
+//  1-Primitive data type
+//number => 2 to power 33 , fro numbers
+// bigint -> for very large numbers
+// string -> " "/ ''
+// undefined
+// symbol -> its is unique
+// 2-
+// object ->// null -> use to store the empty data type
