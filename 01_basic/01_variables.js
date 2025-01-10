9419c9a44f3a596e62596c335233c2303f73001d
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-01-10 16:43:33 +0000

    first commit done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=14456
+let accountEmail = "akashdeep250@gmail.com"
+var accountPassword ="12345"
+
+let accountState ="";
+accountEmail="deepakashdeep527@gmail.com"
+accountPassword="21212343"
+
+
+console.log([accountEmail,accountPassword])
+console.table([accountEmail,accountPassword])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,1 @@
+"use strict";//treat all js ocde as newer version
