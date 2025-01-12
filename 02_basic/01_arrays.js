4541cc7c02f23c93294daf5f70cf84fd726d08fc
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-01-12 04:42:33 +0000

    Array commit

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,40 @@
+// Array
+const myArr=[0,1,2,3,4,5]
+const Heros=["JOhnWIck", "IronMan","hulk"]
+
+const MyArr2= new Array(1,2,3,4,5)
+// console.log(myArr[1]);
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+
+// myArr.pop(3)
+// console.log(myArr);
+
+// myArr.unshift(21)
+// console.log(myArr)
+
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(19));
+
+// const newArr = myArr.join()
+ // Join method join the array and change into string type
+// console.log(newArr);
+
+// // Slice, splice
+// console.log("A",myArr);
+
+// const myn=myArr.slice(1,3)
+// //Slice slices the array into given parts from starting to second last no, it does not include last digit
+// console.log("B",myn);
+
+// const myn2 =myArr.splice(1,3)
+// // Splice manipulates and removes the given no from array , which has  to be splice
+// console.log("C",myArr)
+// console.log(myn2);
+
+
+
