32f5f8b8bcfc805090c8b60e7c0668c8a04e7433
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-01-13 19:40:52 +0000

    function last night commit

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,13 @@
+function sayMyName(){
+    console.log("Akash")
+    console.log("deep")
+}
+sayMyName()
+
+function addTwoNumber(num1,num2){
+    // console.log(num1+num2);
+    let result=num1+num2
+    return result
+}
+const result=addTwoNumber(10,20)
+console.log("Result:",result);
