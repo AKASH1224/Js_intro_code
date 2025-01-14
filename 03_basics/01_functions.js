084f86bd7de56280dddc877ba3a2320b11df1cda
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-01-14 08:15:18 +0000

    function commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -2,12 +2,21 @@ function sayMyName(){
     console.log("Akash")
     console.log("deep")
 }
-sayMyName()
+sayMyName() //sayMyname is refernce and () this is exectuion
+// when we declare the defintion in function it is called parameter. e.g:- num1,num2
 
 function addTwoNumber(num1,num2){
     // console.log(num1+num2);
     let result=num1+num2
+    // when we write the return keyword it return the value, here return result is returning the result value, and then
+    // then storing in the below result, and then printing the value using log
     return result
 }
-const result=addTwoNumber(10,20)
-console.log("Result:",result);
+const result=addTwoNumber(10,20) // when we call the value its is called argument ,e.g :-10,20
+// console.log("Result:",result);
+
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("AKash deep"));
