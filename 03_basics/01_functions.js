1305bddc74f55a22fdfd4590d53e1ed9f7361ed7
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-01-14 08:27:39 +0000

    commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,10 +1,10 @@
 function sayMyName(){
-    console.log("Akash")
-    console.log("deep")
+    // console.log("Akash")
+    // console.log("deep")
 }
 sayMyName() //sayMyname is refernce and () this is exectuion
-// when we declare the defintion in function it is called parameter. e.g:- num1,num2
 
+// when we declare the defintion in function it is called parameter. e.g:- num1,num2
 function addTwoNumber(num1,num2){
     // console.log(num1+num2);
     let result=num1+num2
@@ -17,6 +17,13 @@ const result=addTwoNumber(10,20) // when we call the value its is called argumen
 
 
 function loginUserMessage(username){
+    if(username ===undefined){
+        console.log("Please enter a userName");
+        return
+        
+    }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("AKash deep"));
+// console.log(loginUserMessage("AKash deep"));
+console.log(loginUserMessage());
+//when we return the empty argument to the function, it gives the undefined message
