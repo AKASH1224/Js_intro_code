4aa5bfb84b0e129daa33514b3507686cb2499635
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-01-16 05:16:24 +0000

    control flow commit

---
+++ control_flow/one.js
@@ -0,0 +1,19 @@
+// if 
+// const temperature = 51
+// if (temperature >40) {
+//     console.log(" temparture is greater than 40")
+// }
+// else{
+//     console.log("tmeperature is greater than 51")
+// }
+
+
+// const score =200
+// if(score > 100){
+// let power ="fly "
+// // here we are using let operator because it cannot go outside the scope,and make wrong output, 
+// console.log(`User power: ${power}`)
+// }
+
+// console.log(`User power :${power}`)
+
