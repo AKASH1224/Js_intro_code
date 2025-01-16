894aaf69de4579dadab653442c5a37d40f8b8eb1
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-01-16 09:07:05 +0000

    commit

---
+++ 05_iterations/01_loops.js
@@ -0,0 +1,23 @@
+// For 
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    // console.log(element);
+}
+for(let i=0;i<=10;i++){
+    const element=i;
+    if(element ==5){
+        // console.log("5 is best number")
+    }
+    // console.log(i)
+}
+
+for (let i = 0; i <=10; i++) {
+    const element = i;
+    console.log(`Outer loop Value : ${i}`);
+    for (let j = 0; j <=10; j++) {
+      console.log(`inner loop value ${j} and outer value ${i}`)
+        
+    }
+    
+    
+}
