69fb9ded6d296fc46ce2ad36290c7dbfd49f2ec9
Akash deep <77068840+AKASH1224@users.noreply.github.com>
2025-03-12 05:09:40 +0000

    math

---
+++ Js-practice/Obj_pract.js
@@ -0,0 +1,12 @@
+function rectAngl(a,b){
+    const rectangl={
+        length :a,
+        width  :b,
+        perimeter :2*(a+b),
+        area :a*b
+
+    }
+ return rectangl
+}
+const rect2 = new rectAngl(2,2)
+console.log(rect2)
